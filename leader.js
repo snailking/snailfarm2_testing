63de8ec4003b0c07275fa51bc07ce0333850c017
snailking <39387494+snailking@users.noreply.github.com>
2018-06-23 00:37:04 +0200

    Add files via upload

--- leader.js
+++ leader.js
@@ -36,6 +36,7 @@ var a1 = new Player("0x58e90f6e19563ce82c4a0010cece699b3e1a6723", 0)
 var a2 = new Player("0xe84fd91cbba2fcddc22d96965158ae676dd61d28", 0)
 var a3 = new Player("0x20c945800de43394f70d789874a4dac9cfa57451", 0)
 var a4 = new Player("0x1ec75d8ca166c8f12a3531204923cbcc8f020ed2", 0)
+var a5 = new Player("0x922cFfa33A078B4Cc6077923e43447d8467F8B55", 0)
 
 // Is this the right way to do this? Research best practice to enter lots of variables in arrays
 
@@ -46,7 +47,7 @@ var leaderArray = [a1, a2, a3, a4]
 function updateLeaderboard(){
 	addtext = "";
 	a1.snails = hatcherySnail(a1.address);
-	addtext += a1.address + " has " + a1.snails + " snails <br>"
+	addtext += a1.address + " has " + hatcherySnail(a1.address) + " snails <br>"
 	a2.snails = hatcherySnail(a2.address);
 	addtext += a2.address + " has " + a2.snails + " snails <br>"
 	a3.snails = hatcherySnail(a3.address);
