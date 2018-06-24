610f4bf330382be3b776857ca88848112a6cc95d
snailking <39387494+snailking@users.noreply.github.com>
2018-06-24 17:09:03 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -304,8 +304,8 @@ function updateSquirrelReq(){
 function updateTadpoleReq(){
     var tadpolereqdoc=document.getElementById('tadpolereq')
 	tadpoleReq(function(req) {
-		var tadpoleRoundUp = formatEthValue(web3.fromWei(req,'ether'));
-		tadpoleRoundUp = tadpoleRoundUp + 0.0001;
+		var tadpoleRoundUp = web3.fromWei(req,'ether') + 0.00001;
+		tadpoleRoundUp = formatEthValue(tadpoleRoundUp);
 		tadpolereqdoc.textContent = tadpoleRoundUp;
 	});
 }
